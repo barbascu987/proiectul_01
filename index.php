23b92adff59ad310b83ae65ec01b595b8fbad318
barbascu987 <barbascuvirgil@hotmail.com>
2019-08-27 18:17:06 +0300

    antet si subsol

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<?php include ("componente/footer.php"); ?>   
 <body>
-    
-</body>
-</html>     
+    <div class="row">
+        <div class="demo-heading col-md-12">
+            <h2 class="text-center"></h2>
+        </div>
+        <div class="login-form col-md-4 offset-md-4">
+            <h4>Logare aplicatie FACTURA MEA:</h4>
+            <form method="post" action="">
+                <div class="form-group">
+                    <?php //if ($loginError ) { ?>
+                    <div class="alert alert-warning"><?php //echo $loginError; ?></div>
+                    <?php //} ?>
+                </div>
+                <div class="form-group">
+                    <input name="email" id="email" type="email" class="form-control" placeholder="Email address" autofocus="" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="pwd" placeholder="Password" required>
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="login" class="btn btn-info">Logare</button>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php include ("componente/footer.php"); ?>            
